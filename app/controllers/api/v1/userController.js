0b9f9fc28e4d292215ab041ec536eccb5e6bee2e
Wrendra57 <89427507+Wrendra57@users.noreply.github.com>
2025-03-09 03:26:25 +0700

    fix(user-controller): normalize phone number format (628)

--- app/controllers/api/v1/userController.js
+++ app/controllers/api/v1/userController.js
@@ -2,10 +2,12 @@ const userService=require("../../../services/userService")
 const createLogger = require("../../../utils/logger")
 const logger = createLogger(__filename)
 const template = require('../../../utils/template/templateResponeApi')
+const {convertPhoneNumber} = require("../../../utils/converter/converterPhoneNumber");
 const registerUser = async (req, res) => {
     try {
-        const { name, email, password, phone_number } = req.body;
+        const { name, email, password } = req.body;
 
+        const phone_number = await convertPhoneNumber(req.body.phone_number);
         const registeredUser = await userService.createUser({ name, email, password, phone_number });
 
         return res.status(registeredUser.code).json(template.toTemplateResponseApi(registeredUser));
